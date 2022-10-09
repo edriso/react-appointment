9c4c4e35294ab97def61dfb3ebbff1e609b2780a
edrisso <edrisso71@gmail.com>
2022-10-09 13:19:47 +0200

    build app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from "react";
+
+import { BiCalendar } from "react-icons/bi";
+import AddAppointment from "./components/AddAppointment";
+import Search from "./components/Search";
+import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => setAppointmentList(data));
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  // For searching
+  let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc");
+
+  const filterAppointments = appointmentList
+    .filter((item) => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      );
+    })
+    .sort((a, b) => {
+      let order = orderBy === "asc" ? 1 : -1;
+      return a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+        ? -1 * order
+        : 1 * order;
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App container mx-auto mt-5 font-thin">
+      <h1 className="text-5xl text-center mb-6">
+        <BiCalendar className="inline-block text-red-400 align-top" /> Pet
+        Appointments!
+      </h1>
+
+      <AddAppointment
+        onSendAppointment={(newAppt) =>
+          setAppointmentList([...appointmentList, newAppt])
+        }
+        lastId={appointmentList.reduce(
+          (max, item) => (Number(item.id) > max ? Number(item.id) : max),
+          0
+        )}
+      />
+
+      <Search
+        query={query}
+        onQueryChange={(myQuery) => setQuery(myQuery)}
+        orderBy={orderBy}
+        onOrderByChange={(myOrder) => setOrderBy(myOrder)}
+        sortBy={sortBy}
+        onSortByChange={(mySort) => setSortBy(mySort)}
+      />
+
+      <ul className="divide-y divide-gray-200">
+        {filterAppointments.map((appointment) => (
+          <AppointmentInfo
+            key={appointment.id}
+            appointment={appointment}
+            onDeleteAppointment={(id) =>
+              setAppointmentList(
+                appointmentList.filter((appt) => appt.id !== id)
+              )
+            }
+          />
+        ))}
+      </ul>
+    </main>
   );
 }
 
